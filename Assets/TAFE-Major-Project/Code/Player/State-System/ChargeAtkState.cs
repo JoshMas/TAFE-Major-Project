dc2a682e79ef27b4147632d9715c45324e5e0840
JoshMas <49138669+JoshMas@users.noreply.github.com>
2022-06-30 15:33:23 +1000

    Added empty scenes for menus
    Also started work on charge attacks

---
+++ Assets/TAFE-Major-Project/Code/Player/State-System/ChargeAtkState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeAtkState : AbilityState
+{
+    public override void OnEnter(Player _player)
+    {
+        
+    }
+
+    public override void OnHeavyRelease(Player _player)
+    {
+        
+    }
+}
